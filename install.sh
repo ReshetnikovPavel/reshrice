9a052d6f70d0cf6409b8c200f43f83cdb2f66449
Pavel Reshetnikov <pavelresh@yandex.ru>
2025-06-19 23:43:17 +0500

    Add yay

--- install.sh
+++ install.sh
@@ -14,8 +14,32 @@ function prompt {
 function install {
 	echo 'Updating the system'
 	sudo pacman -Syu
-	echo 'Installing git'
-	sudo pacman -S git
+	echo 'Installing git and base-devel'
+	sudo pacman -S git base-devel
+	install_yay
+}
+
+function install_yay {
+	if ! command -v yay &> /dev/null; then
+		echo "Installing yay AUR helper..."
+		sudo pacman -S --needed git base-devel --noconfirm
+		if [[ ! -d "yay" ]]; then
+			echo "Cloning yay repository..."
+		else
+			echo "yay directory already exists, removing it..."
+			rm -rf yay
+		fi
+
+		git clone https://aur.archlinux.org/yay.git
+
+		cd yay
+		echo "building yay.... yaaaaayyyyy"
+		makepkg -si --noconfirm
+		cd ..
+		rm -rf yay
+	else
+		echo "yay is already installed"
+	fi
 }
 
 
